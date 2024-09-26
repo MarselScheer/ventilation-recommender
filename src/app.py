0da0f2d301e7b1b33b9ecf6da37719b935f36e0c
Marsel Scheer <scheer@freescience.de>
2024-09-26 20:08:11 +0200

    dashboard shows humidity and temperature

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 import json
 from shiny.express import render, input, ui
 
-ui.input_slider("val", "Slider label", min=0, max=100, value=50)
-
 
 def load_data() -> list:
     rtn = []
@@ -14,14 +12,28 @@ def load_data() -> list:
 
 @render.plot
 def humidity_plot():
+    from datetime import datetime
     from matplotlib import pyplot as plt
 
-    data = load_data()
-    y = [e["humidity"] for e in data]
-    fig = plt.plot(range(len(y)), y)
+    data = load_data()[-30:]
+    x = [datetime.strptime(e["timestamp"], "%Y-%m-%d %H:%M:%S.%f") for e in data]
+    h = [e["humidity"] for e in data]
+    t = [e["temperature"] for e in data]
+
+    fig, ax1 = plt.subplots()
+
+    color = "tab:blue"
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("humidity", color=color)
+    ax1.plot(x, h, color=color)
+    ax1.tick_params(axis="y", labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+
+    color = "tab:red"
+    ax2.set_ylabel("temp", color=color)  # we already handled the x-label with ax1
+    ax2.plot(x, t, color=color)
+    ax2.tick_params(axis="y", labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
     return fig
-
-
-@render.text
-def slider_val():
-    return f"Slider value: {input.val()}"
